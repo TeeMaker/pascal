2e1ac219d6f4a8f81e2cf9e54fcccd541eb98c1d
Székely Kolos Mátyás <kolosmatyas@gmail.com>
2022-11-23 13:33:49 +0100

    gyak10 - joke fetch

---
+++ gyak10/gyak10/wwwroot/geek.js
@@ -0,0 +1,22 @@
+﻿var viccek;
+
+window.onload = letoltes()
+
+function letoltes() {
+    fetch('./jokes.json')
+        .then(response => response.json())
+        .then(data => letoltesBef(data))
+
+}
+
+
+function letoltesBef(d) {
+    console.log("Sikeres letoltes!")
+    console.log(d)
+    for (let i = 0; i <= d.length; i++) {
+        ujDiv = document.createElement("div")
+        ujDiv.InnerHTML = JSON.stringify(d[i])
+        document.getElementById("viccek").appendChild(ujDiv)
+    }
+    viccek = d;
+}
